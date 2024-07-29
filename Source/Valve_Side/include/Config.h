432f7ca21437ea922534846362c2d3cdb40d994e
vacmg <70143287+vacmg@users.noreply.github.com>
2024-07-29 18:56:22 +0200

    Disable Debug over serial

--- Source/Valve_Side/include/Config.h
+++ Source/Valve_Side/include/Config.h
@@ -15,7 +15,7 @@
 
 #define VS "V2.0.0"
 
-#define DEBUG 1
+#define DEBUG 0
 #define DEBUGWATCHDOG 0
 #define DISABLE_WATCHDOGS 0 // This will also disable the connection check with the other MCU
 #define MOCK_SENSORS 0
